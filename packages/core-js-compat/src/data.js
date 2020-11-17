8a61225fdc64830a71f7e7ddc08ac12cd7e6f606
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-18 05:53:40 +0700

    remove `esnext.promise.all-settled` (moved to stable es)

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1419,8 +1419,6 @@ const data = {
   'esnext.observable': {
   },
   // TODO: Remove from `core-js@4`
-  'esnext.promise.all-settled': null,
-  // TODO: Remove from `core-js@4`
   'esnext.promise.any': null,
   'esnext.promise.try': {
   },
@@ -1574,7 +1572,6 @@ const data = {
 
 // TODO: Remove from `core-js@4`
 data['esnext.aggregate-error'] = data['es.aggregate-error'];
-data['esnext.promise.all-settled'] = data['es.promise.all-settled'];
 data['esnext.promise.any'] = data['es.promise.any'];
 data['esnext.string.match-all'] = data['es.string.match-all'];
 data['esnext.string.replace-all'] = data['es.string.replace-all'];
