89234a371786906570c3f902033f069dadf4cfdc
Denis Pushkarev <zloirock@zloirock.ru>
2020-11-19 12:32:39 +0700

    remove `esnext.math.{ iaddh, isubh, imulh, umulh }` (withdrawn)

--- packages/core-js-compat/src/data.js
+++ packages/core-js-compat/src/data.js
@@ -1374,15 +1374,6 @@ const data = {
   },
   'esnext.math.fscale': {
   },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.iaddh': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.imulh': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.isubh': {
-  },
   'esnext.math.rad-per-deg': {
   },
   'esnext.math.radians': {
@@ -1392,9 +1383,6 @@ const data = {
   'esnext.math.seeded-prng': {
   },
   'esnext.math.signbit': {
-  },
-  // TODO: Remove from `core-js@4`
-  'esnext.math.umulh': {
   },
   'esnext.number.from-string': {
   },
