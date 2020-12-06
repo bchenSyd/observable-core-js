cda1db8b384df8512f20fc3b3cffd3f14241733d
Denis Pushkarev <zloirock@zloirock.ru>
2020-12-06 18:08:27 +0700

    drop `es.date.to-string`

--- packages/core-js/es/index.js
+++ packages/core-js/es/index.js
@@ -139,7 +139,6 @@ require('../modules/es.math.to-string-tag');
 require('../modules/es.math.trunc');
 require('../modules/es.date.to-json');
 require('../modules/es.date.to-iso-string');
-require('../modules/es.date.to-string');
 require('../modules/es.date.to-primitive');
 require('../modules/es.json.stringify');
 require('../modules/es.json.to-string-tag');
